aef9a6d7546436f6884394696a98d8d2692992e1
R S Verma <ramsahare045@gmail.com>
2026-02-14 01:35:32 +0530

    Initial commit

---
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> employees = new()
+        {
+            new Employee { Id = 1, Name = "John", Department = "IT" },
+            new Employee { Id = 2, Name = "Mary", Department = "HR" }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(employees);
+        }
+    }
+}
