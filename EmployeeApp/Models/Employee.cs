62654d4881bf90640ed1fed1339ee6cfad668f5e
R S Verma <ramsahare045@gmail.com>
2026-02-14 12:20:08 +0530

    my changes

--- EmployeeApp/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeApp.Models
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Department { get; set; } = string.Empty;
-    }
-}
